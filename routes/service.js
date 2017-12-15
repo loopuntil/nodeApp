0fb04d1637d78a0411070f1f851988e103f8f1ee
loopuntil <joy.hyweb@gmail.com>
2017-12-15 11:02:47 +0800

    node restful api demo

--- routes/service.js
+++ routes/service.js
@@ -26,22 +26,15 @@
         { id: '20', name: 'Tornado' }
     ]
 
-    return {
-        getMsg() {
-            return {
-                msg: 'Hello!'
-            }
+    const mockDao = {
+        query() {
+            return heroList
         },
-        getList() {
-            return heroList;
-        },
-        addHero(hero) {
-            console.log('add:' + JSON.stringify(hero))
+        insert(hero) {
             heroList.push(hero)
             return true
         },
-        updateHero(hero) {
-            console.log('up:' + JSON.stringify(hero))
+        updat(hero) {
             let rtn = false
             let list = heroList.map(o => {
                 rtn = true
@@ -53,19 +46,33 @@
             heroList = list
             return rtn
         },
-        removeHero(id) {
-            console.log(`id:${id}`)
+        delete(id) {
             let list = [...heroList]
             let rtn = false;
             let index = list.findIndex(o => o.id == id)
-            console.log(`index:${index} list size:${list.length}`);
             if (index > -1) {
                 list.splice(index, 1);
-                console.log(`bf list size:${list.length}`);
+
                 heroList = list;
                 rtn = true;
             }
             return rtn;
+        }
+
+    }
+
+    return {
+        getList() {
+            return mockDao.query();
+        },
+        addHero(hero) {
+            return mockDao.insert(hero)
+        },
+        updateHero(hero) {
+           return mockDao.updat(hero)
+        },
+        removeHero(id) {
+           return mockDao.delete(id)
         }
     }
 })
