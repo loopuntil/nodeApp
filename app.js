64eb78db2b534dd0d1e5de246eb517a701212c8d
loopuntil <joy.hyweb@gmail.com>
2017-12-29 11:43:48 +0800

    add note

--- app.js
+++ app.js
@@ -7,17 +7,21 @@ var bodyParser = require('body-parser');
 
 var index = require('./routes/index');
 var api = require('./routes/api');
+//因為前端可能是另一台Server，所以要載入cors(Cross-Origin Resource Sharing)套件
 var cors = require('cors')
 var app = express();
+//cors參數設定
 const corsOptions = {
   origin: [
-    'http://localhost:4200',
+    'http://localhost:8080',
+    'http://localhost:4200'
   ],
   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS'
 };
-
+//使用cors
 app.use(cors(corsOptions))
 
+//view 使用的模板引擎，api可忽略
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
@@ -25,12 +29,15 @@ app.set('view engine', 'jade');
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
+//下面兩行是接收json格式傳入的參數
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
+//設定可靜態存取的目錄，api可忽略
 app.use(express.static(path.join(__dirname, 'public')));
-
+//路由，跟目錄
 app.use('/', index);
+//api路由
 app.use('/api', api);
 
 // catch 404 and forward to error handler
--- routes/api.js
+++ routes/api.js
@@ -1,10 +1,18 @@
 const express = require('express')
 const router = express.Router()
+//service實務上要改寫連真正的db，這邊是範例所以用mock的
 let service = require('./service')
+//如果app.js沒有設定要加下面這三行接收JSON格式參數，有就不用加了
 /*
 const bodyParser = require('body-parser')
 router.use(bodyParser.urlencoded({ extended: false }))
 router.use(bodyParser.json())
+*/
+
+/* 
+domain/api/hero
+如果在本機的話就是http://localhost:3000/api/hero
+使用get查詢，post新增，回傳格式包成json
 */
 router.route('/hero')
   .get((req, res) => {
@@ -21,6 +29,9 @@ router.route('/hero')
     res.json(obj)
   })
 
+/* 
+在網址帶入id，使用put修改，delete刪除
+*/
 router.route('/hero/:id')
   .put((req, res) => {
     let hero = {
